bb7a44590bce5824184cf1befceb295e499fa824
Carles <4223148+cmmata@users.noreply.github.com>
2022-01-07 10:31:09 +0100

    Added a dropdown to test if it is working

--- pages/index.vue
+++ pages/index.vue
@@ -2,26 +2,37 @@
   <el-main>
     <el-row>
       <el-col :span="4">
+        <el-dropdown split-button type="primary" @click="handleClick">
+          Dropdown List
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item>Action 1</el-dropdown-item>
+              <el-dropdown-item>Action 2</el-dropdown-item>
+              <el-dropdown-item>Action 3</el-dropdown-item>
+              <el-dropdown-item>Action 4</el-dropdown-item>
+              <el-dropdown-item>Action 5</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
       </el-col>
       <el-col :span="16">
         <div class="main-content">
           <Articles />
         </div>
       </el-col>
-      <el-col :span="4">
-      </el-col>
+      <el-col :span="4"> </el-col>
     </el-row>
   </el-main>
 </template>
 
 <script>
-import Articles from '../components/Articles'
+import Articles from "../components/Articles";
+import { ArrowDown } from "@element-plus/icons-vue";
+
 export default {
-  name: 'PortalIndex',
-  components: { Articles }
-}
+  name: "PortalIndex",
+  components: { Articles, ArrowDown },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
